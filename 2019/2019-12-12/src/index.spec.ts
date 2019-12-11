7247e8beac2cd22ce627c25a3ca721a2a2beed4a
Lukáš Bednařík <l.bednarik@gmail.com>
2019-12-11 13:48:50 +0100

    Empty typescript project

---
+++ 2019/2019-12-12/src/index.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+function f() {
+    return 0;
+}
+
+describe('Kata XXX', () => {
+    it('Function returns 0', () => {
+        expect(f()).to.equal(0)
+    });
+});
